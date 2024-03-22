4fa5748b8e17511a159431968d649f1e8d1510e2
Shailendra Jain <shailendrajain@Shailendras-MacBook-Pro.local>
2024-03-22 17:44:48 +0000

    updated solution for lcs

--- lcs.py
+++ lcs.py
@@ -7,13 +7,17 @@ def lcs(s1, s2, m, n):
     else:
         return max(lcs(s1, s2, m-1, n), lcs(s1, s2, m, n-1))
 
-def solve(s1, s2, m, n, memo):
+def solve_recur_dp(s1, s2, m, n, memo):
     if m == 0 or n == 0:
         return 0
+
+    if memo[m][n] != -1:
+        return memo[m][n]
+
     if s1[m-1] == s2[n-1]:
-        memo[m][n]=1+solve(s1, s2, m-1, n-1, memo)
+        memo[m][n]=1+solve_recur_dp(s1, s2, m-1, n-1, memo)
     else:
-        memo[m][n]=max(solve(s1, s2, m-1, n, memo), solve(s1, s2, m, n-1, memo))
+        memo[m][n]=max(solve_recur_dp(s1, s2, m-1, n, memo), solve_recur_dp(s1, s2, m, n-1, memo))
     return memo[m][n]
 
 def lcs_tab(s1, s2, m, n, tab):
@@ -23,7 +27,6 @@ def lcs_tab(s1, s2, m, n, tab):
                 tab[i][j] = 1 + tab[i-1][j-1]
             else:
                 tab[i][j] = max(tab[i-1][j], tab[i][j-1])
-    print(tab)
     return tab
 
 def lcs_memo(s1, s2, m, n):
@@ -33,15 +36,15 @@ def lcs_memo(s1, s2, m, n):
     for j in range(n+1):
         memo[0][j] = 0
     # print(memo)
-    # solve(s1, s2, m, n, memo)
-    # print([print(*line) for line in memo])
-    # print(f"lcs is : {memo[m][n]}")
+    solve_recur_dp(s1, s2, m, n, memo)
+    print(f"print table : {[print(*line) for line in memo]}")
+    print(f"lcs is : {memo[m][n]}")
     print("*"*50)
     print(lcs_tab(s1, s2, m, n, memo))
     print([print(*line) for line in memo])
 
 s1 = "bit" # input("Enter first string: ")
 s2 = "bat" # input("Enter Second string: ")
-ln = lcs(s1, s2, len(s1), len(s2))
-print(f"lcs of {s1} and {s2} is : {ln}")
+# ln = lcs(s1, s2, len(s1), len(s2))
+# print(f"lcs of {s1} and {s2} is : {ln}")
 lcs_memo(s1, s2, len(s1), len(s2))
